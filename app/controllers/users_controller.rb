a51c12a01e2ad457166f12e34eb636de16243ccf
Josh Sparkes <jshsparx@gmail.com>
2022-05-05 11:16:33 -0600

    change redirects in controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,9 @@ def create
     user = User.new(user_params)
   if user.save
     session[:user_id] = user.id
-    redirect_to '/'
+    redirect_to root_path
   else
-    redirect_to '/signup'
+    redirect_to signup_path
   end
   end
 end
