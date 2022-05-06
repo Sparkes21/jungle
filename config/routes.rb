b4565ced6e43f14cfcc20d0e99f67f8ad1601ad1
Josh Sparkes <jshsparx@gmail.com>
2022-05-05 19:41:37 -0600

    clean up routes.rb a little

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'users/new'
-  get 'users/create'
-  get 'about/index'
-  get 'static_pages/help'
-  get 'static_pages/faq'
-  get 'static_pages/about'
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  get '/logout' => 'sessions#destroy'
-  get '/signup' => 'users#new'
-  post '/users' => 'users#create'
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
@@ -30,6 +16,13 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :update, :show, :destroy]
   end
+
+  get 'about/index'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
