2297c001c80d52920d7627039b5e7a0dc924d978
Josh Sparkes <jshsparx@gmail.com>
2022-05-10 15:05:12 -0600

    trying to fix signup/login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,11 +11,12 @@ def create
     redirect_to signup_path
   end
   end
-end
 
-private
 
-def user_params
-  params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
 end
 
---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,10 @@
+describe('jungle homepage', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  });
+  it("Should display the homepage", () => {});
+
+  it("Should redirect to product detail page", () => {
+    
+  })
+});
