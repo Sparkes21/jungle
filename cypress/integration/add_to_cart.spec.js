b9b27c6c1cbf669d3b773e1ad505daa0cac7d090
Josh Sparkes <jshsparx@gmail.com>
2022-05-10 17:25:06 -0600

    add test for count in cart

---
+++ cypress/integration/add_to_cart.spec.js
@@ -0,0 +1,9 @@
+describe('jungle homepage', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  });
+  it("Should display the homepage", () => {});
+  
+  it("Should change the count of the cart button when adding products to it", () => {
+  });
+});
